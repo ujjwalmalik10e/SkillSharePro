19398bd6210c8c3ecffeadd86702645a8a5444db
Ujjwal Malik <your-email@example.com>
2026-07-09 00:34:34 +0530

    Allow Vercel frontend in CORS

--- skillshare-pro-backend/server.js
+++ skillshare-pro-backend/server.js
@@ -14,6 +14,7 @@ dotenv.config();
 const app = express();
 app.use(cors({
   origin: "http://localhost:3000",
+  "https://skillshare-frontend-nu.vercel.app",
   credentials: true,
 }));
 app.use(express.json());
